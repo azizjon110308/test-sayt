29e1433d170c7cced3f4573168597fdc7f3c0ecc
azizjon110308 <113838996+azizjon110308@users.noreply.github.com>
2023-01-05 14:49:44 +0500

    a

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from environs import Env
+# from environs import Env
 
-env = Env()
-env.read_env()
+# env = Env()
+# env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
