3a199bb6bec8e6656d75ebaec440fa07c071feea
azizjon110308 <113838996+azizjon110308@users.noreply.github.com>
2023-01-05 14:35:31 +0500

    s

--- onlinexam/settings.py
+++ onlinexam/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'quizsayt',
-        'USER': 'postgres',
+        'USER': 'quizsayt_user',
         'PASSWORD': '19812008',
         'HOST': '127.0.0.1',
         'PORT': '5432',
